69e9be09c0b6dad92e6edafc09e2849a46771852
AbhiramNetha <abhiramnetha17@gmail.com>
2025-09-12 23:19:25 +0530

    Time: 4 ms (78.65%), Space: 44.4 MB (65.82%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,37 +9,54 @@
  * }
  */
 class Solution {
-    private ListNode mergelist(ListNode l1,ListNode l2){
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
-        while(l1 != null && l2 != null){
-            if(l1.val < l2.val){
-                temp.next = l1;
-                temp = temp.next;
-                l1 = l1.next;
-            }
-            else{
-                temp.next = l2;
-                temp = temp.next;
-                l2 = l2.next;
-            }
-        }
-        if(l1!=null) temp.next = l1;
-        if(l2 != null) temp.next = l2;
-        return dummy.next;
-    }
+    // private ListNode mergelist(ListNode l1,ListNode l2){
+    //     if(l1 == null) return l2;
+    //     if(l2 == null) return l1;
+    //     ListNode dummy = new ListNode(-1);
+    //     ListNode temp = dummy;
+    //     while(l1 != null && l2 != null){
+    //         if(l1.val < l2.val){
+    //             temp.next = l1;
+    //             temp = temp.next;
+    //             l1 = l1.next;
+    //         }
+    //         else{
+    //             temp.next = l2;
+    //             temp = temp.next;
+    //             l2 = l2.next;
+    //         }
+    //     }
+    //     if(l1!=null) temp.next = l1;
+    //     if(l2 != null) temp.next = l2;
+    //     return dummy.next;
+    // }
     public ListNode mergeKLists(ListNode[] lists) {
-        int n = lists.length;
-        if(n==0){
-            return null;
+        // int n = lists.length;
+        // if(n==0){
+        //     return null;
+        // }
+        // ListNode head = lists[0];
+        // for(int i=1;i<n;i++){
+        //     ListNode temp = lists[i];
+        //     head = mergelist(head,temp);
+        // }
+        // return head;
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((a,b)-> a.val - b.val);
+        for(ListNode list:lists){
+            if(list != null){
+                heap.add(list);
+            }
         }
-        ListNode head = lists[0];
-        for(int i=1;i<n;i++){
-            ListNode temp = lists[i];
-            head = mergelist(head,temp);
+        ListNode head = new ListNode(-1);
+        ListNode temp = head;
+        while(!heap.isEmpty()){
+            ListNode node = heap.poll();
+            temp.next = node;
+            temp = temp.next;
+            if(node.next != null){
+                heap.add(node.next);
+            }
         }
-        return head;
+        return head.next;
     }
 }
