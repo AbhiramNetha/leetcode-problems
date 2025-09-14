c50ff03f0bcc1ed8cf88fca6269f3eae293fcfc1
AbhiramNetha <abhiramnetha17@gmail.com>
2025-09-14 20:05:43 +0530

    Time: 9 ms (96.28%), Space: 57.7 MB (5.66%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -19,36 +19,77 @@ class Solution {
     //     }
     //     return head;
     // }
-    private int len(ListNode head){
-        ListNode temp = head;
-        int cnt =0;
-        while(temp != null){
-            cnt++;
-            temp = temp.next;
+    // private int len(ListNode head){
+    //     ListNode temp = head;
+    //     int cnt =0;
+    //     while(temp != null){
+    //         cnt++;
+    //         temp = temp.next;
+    //     }
+    //     return cnt;
+    // }
+    private ListNode Merge(ListNode l1,ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while(l1!=null && l2 != null){
+            if(l1.val < l2.val){
+                temp.next = l1;
+                temp = l1;
+                l1 = l1.next;;
+            }
+            else{
+                temp.next = l2;
+                temp = l2;
+                l2 = l2.next;
+            }
         }
-        return cnt;
+        if(l1!=null) temp.next = l1;
+        if(l2 != null) temp.next = l2;
+        return dummy.next;
+    }
+    private ListNode middle(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast!=null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+        
     }
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null){
             return head;
         }
-        ListNode temp = head;
-        int n = len(temp);
-        int[] arr = new int[n];
         
-        for(int i=0;i<n;i++){
-            arr[i] = temp.val;
-            temp = temp.next;
-        }
-        Arrays.sort(arr);
-        // ListNode newhead = head;
-        temp = head;
-        int i=0;
-        while(temp!=null){
-            temp.val = arr[i];
-            temp = temp.next;
-            i=i+1;
-        }
-        return head;
+        ListNode mid =  middle(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+        left = sortList(left);
+        right = sortList(right);
+        return Merge(left,right);
+        // ListNode temp = head;
+        // int n = len(temp);
+        // int[] arr = new int[n];
+        
+        // for(int i=0;i<n;i++){
+        //     arr[i] = temp.val;
+        //     temp = temp.next;
+        // }
+        // Arrays.sort(arr);
+        // // ListNode newhead = head;
+        // temp = head;
+        // int i=0;
+        // while(temp!=null){
+        //     temp.val = arr[i];
+        //     temp = temp.next;
+        //     i=i+1;
+        // }
+        // return head;
+
     }
 }
