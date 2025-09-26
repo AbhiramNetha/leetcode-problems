2a4bba6f5b60969a0a97be11da1795d70f59ec57
AbhiramNetha <abhiramnetha17@gmail.com>
2025-09-26 13:13:27 +0530

    Time: 0 ms (100%), Space: 46.6 MB (40.15%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -26,23 +26,50 @@ class Solution {
         // InOrder to fing that we need prefix sum and also suffix sum, so by using these , we can get the water logged in between the poles
 
         //Finding the prefix sum
-        int n = height.length;
-        int[] prefix = new int[n];
-        prefix[0] = height[0];
-        for(int i=1;i<n;i++){
-            prefix[i] = Math.max(prefix[i-1],height[i]);
-        }
-        //Finding the suffix sum
-        int[] suffix = new int[n];
-        suffix[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i] = Math.max(suffix[i+1],height[i]);
-        }
+        // int n = height.length;
+        // int[] prefix = new int[n];
+        // prefix[0] = height[0];
+        // for(int i=1;i<n;i++){
+        //     prefix[i] = Math.max(prefix[i-1],height[i]);
+        // }
+        // //Finding the suffix sum
+        // int[] suffix = new int[n];
+        // suffix[n-1] = height[n-1];
+        // for(int i=n-2;i>=0;i--){
+        //     suffix[i] = Math.max(suffix[i+1],height[i]);
+        // }
 
-        int ans =0;
-        for(int i=0;i<n;i++){
-            if(prefix[i] > height[i] && suffix[i] > height[i]){
-                ans += Math.min(prefix[i] , suffix[i]) - height[i];
+        // int ans =0;
+        // for(int i=0;i<n;i++){
+        //     if(prefix[i] > height[i] && suffix[i] > height[i]){
+        //         ans += Math.min(prefix[i] , suffix[i]) - height[i];
+        //     }
+        // }
+        // return ans;
+        int n = height.length;
+        int leftmax=0;
+        int rightmax =0;
+        int ans=0;
+        int l=0;
+        int r =n-1;
+        while(l<=r){
+            if(height[l] < height[r]){
+                if(leftmax>height[l]){
+                    ans+= (leftmax - height[l]);
+                }
+                else{
+                    leftmax = height[l];
+                }
+                l++;
+            }
+            else{
+                if(rightmax > height[r]){
+                    ans+= (rightmax - height[r]);
+                }
+                else{
+                    rightmax = height[r];
+                }
+                r--;
             }
         }
         return ans;
