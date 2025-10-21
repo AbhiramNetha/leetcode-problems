a3931d8ddcd715d1825593fb2b30bf2131b157f1
AbhiramNetha <abhiramnetha17@gmail.com>
2025-10-21 12:04:52 +0530

    Time: 34 ms (31.57%), Space: 59.5 MB (43.89%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,19 +1,30 @@
 class Solution {
-    public int findmax(int[] nums,int i , int j){
-        int res = Integer.MIN_VALUE;
-        for(int k=i;k<=j;k++){
-            res = Math.max(res,nums[k]);
-        }
-        return res;
-    }
+    // public int findmax(int[] nums,int i , int j){
+    //     int res = Integer.MIN_VALUE;
+    //     for(int k=i;k<=j;k++){
+    //         res = Math.max(res,nums[k]);
+    //     }
+    //     return res;
+    // }
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        if(n == 1 || k>n){
-            return nums;
-        }
+        if(n==0 || k==0) return new int[0];
+        Deque<Integer> dq = new ArrayDeque<>();
         int[] ans = new int[n-k+1];
-        for(int i=0;i<n-k+1;i++){
-            ans[i] = findmax(nums,i,i+k-1);
+        int j =0;
+        for(int i=0;i<n;i++){
+            while(!dq.isEmpty() && dq.peekFirst() <= i-k){
+                dq.pollFirst();
+            }
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i] ){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            if(i>=k-1){
+                ans[j] = nums[dq.peekFirst()];
+                j++;
+            }
+            
         }
         return ans;
 
@@ -26,6 +37,15 @@ class Solution {
 
 
 
+        // int n = nums.length;
+        // if(n == 1 || k>n){
+        //     return nums;
+        // }
+        // int[] ans = new int[n-k+1];
+        // for(int i=0;i<n-k+1;i++){
+        //     ans[i] = findmax(nums,i,i+k-1);
+        // }
+        // return ans;
 
 
 
