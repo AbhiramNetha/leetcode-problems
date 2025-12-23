160e4da0677a72fd566cfdcf5bb92271397d7dd5
AbhiramNetha <abhiramnetha17@gmail.com>
2025-12-23 13:22:36 +0530

    Time: 0 ms (100%), Space: 46.6 MB (5.22%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,37 +1,36 @@
 class Solution {
-    public boolean isvalidFun(int[][] matrix ,int y,int x){
-        int r = matrix.length;
-        int c = matrix[0].length;
-        boolean bool = true;
-        int i=y;
-        int j=x;
-        int temp = matrix[i][j];
+    public boolean isvalid(int[][] arr,int y, int x){
+        int r = arr.length;
+        int c = arr[0].length;
+        int i = x;
+        int j =y;
+        int tem = arr[i][j];
         while(i<r && j<c){
-            if(matrix[i][j]!=temp){
-                bool = false;
+            if(tem != arr[i][j]){
+                return false;
             }
             i++;
-            j++;
+            j++; 
         }
-        return bool; 
-
+        return true;
     }
     public boolean isToeplitzMatrix(int[][] matrix) {
         int r = matrix.length;
         int c = matrix[0].length;
-        boolean ans = true;
-        for(int j=0;j<c;j++){
-            boolean temp = isvalidFun(matrix,0,j);
+        boolean temp = true;
+        for(int i=0;i<r;i++){
+            temp = isvalid(matrix,0,i);
             if(!temp){
-                ans = false;
+                return false;
+            }
+        } 
+        for(int i=1;i<c;i++){
+            temp = isvalid(matrix,i,0);
+            if(!temp){
+                return false;
             }
         }
-        for(int i=1;i<r;i++){
-            boolean temp = isvalidFun(matrix,i,0);
-            if(!temp){
-                ans = false;
-            }
-        }
-        return ans;
+        return temp;
+
     }
 }
