f9930c214bfa8d6cf27ec70a47e0cdf490226e07
AbhiramNetha <abhiramnetha17@gmail.com>
2026-01-16 00:49:44 +0530

    Time: 10 ms (60.97%), Space: 46.4 MB (35.47%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,10 +1,10 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int n = s.length();
-        int l =0;
-        int maxf= 0;
-        int ans =0;
         int[] hash = new int[26];
+        int l=0;
+        int ans =0;
+        int maxf =0;
         for(int r=0;r<n;r++){
             char ch = s.charAt(r);
             hash[ch-'A']++;
@@ -15,7 +15,7 @@ class Solution {
                 l++;
             }
             if((r-l+1) - maxf <= k){
-                ans = Math.max(ans,r-l+1);
+                ans = Math.max(ans, r-l+1);
             }
         }
         return ans;
