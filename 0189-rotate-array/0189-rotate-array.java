8d93f0055032031b4db135e0d86ad87ca1b0c560
AbhiramNetha <abhiramnetha17@gmail.com>
2026-01-23 12:07:09 +0530

    Time: 0 ms (100%), Space: 61.5 MB (29.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,50 +1,18 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int[] res = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            res[i] = nums[i];
+    public void reverse(int[] arr,int l, int r){
+        while(l<=r){
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
         }
-        for(int i=0;i<nums.length;i++){
-            int nind = (i+k)%nums.length;
-            nums[nind] = res[i];
-        }
-        
-
-        // int n =nums.length;
-        
-        // k = k%n;
-        // int[] left = new int[k];
-        // int[] right = new int[n-k];
-        // for(int i=0;i<k;i++){
-        //     left[i] = nums[i];
-        // }
-        // for(int i=k;i<(n-k);i++){
-        //     left[i] = nums[i];
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(i>k){
-        //         nums[i] = right[i];
-        //     }
-        //     else{
-        //         nums[i]=left[i];
-        //     }
-        // }
-
-
-    //     int n = nums.length;
-    //     k%=n;
-    //     reverse(nums,0,n-1);
-    //     reverse(nums,0,k-1);
-    //     reverse(nums,k,n-1);
-    // }
     }
-    // private void reverse(int[] nums,int l,int r){
-    //     while(l<r){
-    //         int temp = nums[l];
-    //         nums[l] = nums[r];
-    //         nums[r] = temp;
-    //         l++;
-    //         r--;
-    //     }
-    // }
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k=k%n;
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-k-1);
+        reverse(nums,0,n-1);
+    }
 }
