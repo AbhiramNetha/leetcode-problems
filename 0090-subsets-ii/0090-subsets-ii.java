1cb6a993771d5e865b136d814ca8cf503af09a0a
AbhiramNetha <abhiramnetha17@gmail.com>
2026-01-31 12:24:58 +0530

    Time: 2 ms (99.33%), Space: 45.1 MB (55.57%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,13 +1,10 @@
 class Solution {
     public void fun(int ind,int[] arr,List<Integer> list,List<List<Integer>> ans){
-        if(ind == arr.length){
-            return;
-        }
+        ans.add(new ArrayList<>(list));
         for(int i=ind;i<arr.length;i++){
             if(i>ind && arr[i] == arr[i-1]) continue;
             list.add(arr[i]);
             fun(i+1,arr,list,ans);
-            ans.add(new ArrayList<>(list));
             list.remove(list.size()-1);
         }
     }
@@ -16,7 +13,6 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         fun(0,nums,list,ans);
-        ans.add(list);
         return ans;
     }
 }
