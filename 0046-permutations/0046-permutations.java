4a65d010e4be439daf56627b8a89d551ba1c9bc8
AbhiramNetha <abhiramnetha17@gmail.com>
2026-02-08 01:24:03 +0530

    Time: 1 ms (86.9%), Space: 45.6 MB (44.73%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,44 @@
 class Solution {
-    public void swap(int i,int j,int[] arr){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    public void fun(int ind,int[] arr,List<List<Integer>> ans){
-        if(ind==arr.length){
-            ArrayList<Integer> list = new ArrayList<>();
-            for(int i=0;i<arr.length;i++){
-                list.add(arr[i]);
-            }
+    // public void swap(int i,int j,int[] arr){
+    //     int temp = arr[i];
+    //     arr[i] = arr[j];
+    //     arr[j] = temp;
+    // }
+    // public void fun(int ind,int[] arr,List<List<Integer>> ans){
+    //     if(ind==arr.length){
+    //         ArrayList<Integer> list = new ArrayList<>();
+    //         for(int i=0;i<arr.length;i++){
+    //             list.add(arr[i]);
+    //         }
+    //         ans.add(new ArrayList<>(list));
+    //         return;
+    //     }
+    //     for(int i=ind;i<arr.length;i++){
+    //         swap(i,ind,arr);
+    //         fun(ind+1,arr,ans);
+    //         swap(i,ind,arr);
+    //     }
+    // }
+    public void fun(int[] arr,boolean[] freq,List<Integer> list,List<List<Integer>> ans){
+        if(arr.length == list.size()){
             ans.add(new ArrayList<>(list));
             return;
         }
-        for(int i=ind;i<arr.length;i++){
-            swap(i,ind,arr);
-            fun(ind+1,arr,ans);
-            swap(i,ind,arr);
+        for(int i=0;i<arr.length;i++){
+            if(!freq[i]){
+                freq[i] = true;
+                list.add(arr[i]);
+                fun(arr,freq,list,ans);
+                list.remove(list.size()-1);
+                freq[i] = false;
+            }
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        fun(0,nums,ans);
+        List<Integer> list = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        fun(nums,freq,list,ans);
         return ans;
     }
 }
