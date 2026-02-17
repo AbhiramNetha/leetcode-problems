ce67f3a1d42756e621963fc26cc8dd0a04240e43
AbhiramNetha <abhiramnetha17@gmail.com>
2026-02-17 16:55:04 +0530

    Time: 2 ms (93.88%), Space: 47.5 MB (78.67%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,24 +1,18 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] ans = new int[2];
-        int n = nums.length;
-        HashSet<Integer> hs = new HashSet<>();
-        int dup = 0;
+        int n  =nums.length;
+        long s=0,s2=0;
+        long sn = (long)n*(n+1)/2;
+        long s2n = (long)n*(n+1)*(2*n+1)/6;
         for(int i=0;i<n;i++){
-            if(!hs.contains(nums[i])){
-                hs.add(nums[i]);
-            }
-            else{
-                dup = nums[i];
-            }
+            s+= nums[i];
+            s2+= (long)nums[i] * (long)nums[i];
         }
-        int mis = 0;
-        for(int i=1;i<=n;i++){
-            if(!hs.contains(i)){
-                mis = i;
-            }
-        }
-        
-        return new int[]{dup,mis};
+        long val1 = s-sn;
+        long val2 = s2 - s2n;
+        val2 = val2/val1;
+        long x = (val1 + val2)/2;
+        long y = val2-x;
+        return new int[]{(int)x,(int)y};
     }
 }
